d843a1cdb25760ff5788e1f1619152a088e05115
natestet <70996887+natestet@users.noreply.github.com>
2020-10-28 14:35:57 -0700

    Update calc.js
    restructured function and decreased the number of input as per the professor's recommendation.
    Added if/else-if statement.

--- calc.js
+++ calc.js
@@ -1,15 +1,20 @@
-function calcTotalSeats(){
-	var roomArea = document.getElementById('userForm');
-	var numTables = 0;
-	var tableLength = 0;
-	var tableWidth = 0;
-	var numSeats = 0;
-}
+function socialDistancingCheck(){
+	
+	//Prompt user for initial inputs
+	var numTables = window.prompt("Please enter the number of tables (in feet):");
+	var numSeats = window.prompt("Please enter the number of seats at each table (in feet):");
+	var DINING_ROOM_AREA = 1000;
+	//tables are 4ftx4ft. To calculate the actual area of the table, 6 feet must be added to each dimension, resulting in an area value of 100 ft^2
+	var TABLE_AREA = 100;
+	var spareRoom = DINING_ROOM_AREA - (numTables*TABLE_AREA);
+	var tableRemoval = numTables-10;
+	var totalSeats = numSeats*numTables;
+	var seatsAfterTableRemoval = (numTables*numSeats) - (tableRemoval*numSeats);
 
-/*
-- Add 6 feet onto both tableWidth and tableLength
-- Calculate area with new demensions
-- Calculate how many tables can fit in the dining room
-  while the area of the tables is < area of the dining room
-- Print how many tables can fit in dining room
-*/
+	if(numTables*TABLE_AREA<=DINING_ROOM_AREA){
+		window.alert("You can fit your tables in the dining room while maintaining social distancing! You have " + spareRoom + " feet to spare and you can seat " + totalSeats + "people.");
+	}else if(numTables*TABLE_AREA > DINING_ROOM_AREA){
+		window.alert("You cannot fit all your tables in the dining room while maintaing social distancing. To maintain social distancing, you have to remove " + tableRemoval + " tables and then you will be able to seat " + seatsAfterTableRemoval + " people.");
+	}
+	
+}
