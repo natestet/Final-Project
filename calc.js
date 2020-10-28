e8a81634690b38a8c26171e5c67387964e782bc4
natestet <70996887+natestet@users.noreply.github.com>
2020-10-27 23:44:44 -0700

    Update calc.js
    add variable declarations and initializations and add comment about math logic

--- calc.js
+++ calc.js
@@ -1,1 +1,15 @@
+function calcTotalSeats(){
+	var roomArea = document.getElementById('userForm');
+	var numTables = 0;
+	var tableLength = 0;
+	var tableWidth = 0;
+	var numSeats = 0;
+}
 
+/*
+- Add 6 feet onto both tableWidth and tableLength
+- Calculate area with new demensions
+- Calculate how many tables can fit in the dining room
+  while the area of the tables is < area of the dining room
+- Print how many tables can fit in dining room
+*/
